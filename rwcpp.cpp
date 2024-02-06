fad55d10e05c8f94e333ae146a798af99d50988b
Jeremy Lee <mengchil@cs.cmu.edu>
2024-02-06 17:55:06 -0500

    Update rwcpp.cpp

--- rwcpp.cpp
+++ rwcpp.cpp
@@ -7,8 +7,6 @@
 #include <tuple>
 #include <omp.h>
 
-// g++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) rwcpp.cpp -o rwcpp$(python3-config --extension-suffix)
-
 
 namespace std{
     namespace
